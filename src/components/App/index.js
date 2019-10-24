542f77e8e1cd8a0e4787871b679daec04e5b73db
VK <vitaliy.krynytskyy@gmail.com>
2019-10-24 11:32:57 +0100

    Added ability to create URL links

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import './App.css';
 
 import LinkList from '../LinkList';
+import CreateShortLink from '../CreateShortLink';
 
-function App() {
-    return <LinkList />;
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <h2>All Links</h2>
+                    <LinkList />
+                </div>
+                <div>
+                    <h2>Create a short URL</h2>
+                    <CreateShortLink />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/CreateShortLink/index.js
+++ src/components/CreateShortLink/index.js
@@ -1,15 +1,54 @@
 import React from 'react';
 
 import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import { graphql, withApollo } from 'react-apollo';
 
 const CREATE_SHORT_LINK = gql`
-    mutation CreateLinkMutation($url: String!, $description: String!, $createdById: ID!) {
-        createLink(url: $url, description: $description, createdById: $createdById) {
+    mutation createLinkMutation($url: String!, $description: String!, $hash: String!) {
+        createLink(url: $url, description: $description, hash: $hash) {
             id
+            hash
         }
     }
 `;
+
+const GET_LINK_COUNT = gql`
+    query getLinkCount {
+        links: _allLinksMeta {
+            count
+        }
+    }
+`;
+
+const createHash = itemCount => {
+    let hashDigits = [];
+    // dividend is a unique integer (in our case, number of links)
+    let dividend = itemCount + 1;
+    let remainder = 0;
+
+    while (dividend > 0) {
+        remainder = dividend % 62;
+        dividend = Math.floor(dividend / 62);
+        hashDigits.unshift(remainder);
+
+    }
+    console.log(hashDigits);
+
+    const alphabetArray = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`.split('');
+
+    // Convert hashDigits to base62 representation
+    let hashString = '';
+    let i = 0;
+
+
+    while (hashDigits.length > i) {
+        hashString += alphabetArray[hashDigits[i]];
+        i++;
+    }
+
+    return hashString;
+
+};
 
 class CreateShortLink extends React.Component {
     constructor(props) {
@@ -22,7 +61,22 @@ class CreateShortLink extends React.Component {
     }
 
     createShortLink = async () => {
-        // we create a short link here
+        const linkCountQuery = await this.props.client.query({
+            query: GET_LINK_COUNT,
+        });
+
+        const linkCount = linkCountQuery.data.links.count;
+        const hash = createHash(linkCount);
+
+        const { url, description } = this.state;
+
+        await this.props.createShortLinkMutation({
+            variables: {
+                url,
+                description,
+                hash
+            }
+        });
     };
 
     render() {
@@ -50,4 +104,4 @@ class CreateShortLink extends React.Component {
     }
 }
 
-export default graphql(CREATE_SHORT_LINK, { name: 'createShortLinkMutation' })(CreateShortLink);
+export default graphql(CREATE_SHORT_LINK, { name: 'createShortLinkMutation' })(withApollo(CreateShortLink));
